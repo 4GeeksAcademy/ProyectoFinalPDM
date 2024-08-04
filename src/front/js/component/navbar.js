567041c803d0343ba7d01c6da9e1a9dca7ba5ab0
MariaVictoriaZ <163335173+MariaVictoriaZ@users.noreply.github.com>
2024-08-04 14:01:14 +0000

    se trajeron los cambios de develop

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -23,28 +23,6 @@ export const Navbar = () => {
         <Link to="/Register">
           <button className="buttonN ms-2">Register</button>
         </Link>
-        
-<Link to={"/RegisterDate"}>
-<		button> RegisterDate </button>
-</Link>
-<Link to={"/CreateService"}>
-<button className="ms-3 me-3"> CreateService </button>
-</Link>
-<Link to={"/"}>
-<button className="ms-3 me-3"> Home </button>
-</Link>
-<Link to={"/Message"}>
-<button className="ms-3 me-3"> Message </button>
-</Link>
-<Link to={"/PerfilUsuario"}>
-<button> Perfil Usuario</button>
-</Link>
-<Link to={"/AgendarCita"}>
-<button className="ms-3 me-3"> Agendar Cita</button>
-</Link>
-<Link to={"/Agenda"}>
-<button className="ms-3 me-3"> Agenda</button>
-// </Link>
       </div>
     </nav>
   );
