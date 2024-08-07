117ca8cfa5df845a005a8e38d1250c4f9aae8f15
MariaVictoriaZ <163335173+MariaVictoriaZ@users.noreply.github.com>
2024-08-07 00:46:34 +0000

    se intento modificar el footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -4,13 +4,7 @@ import '/workspaces/ProyectoFinalPDM/src/front/styles/Footer.css';
 export const Footer = () => (
   <footer className="footer">
     <div className="footer-container">
-      <p>
-        Made with <i className="fa fa-heart" /> by{" "}
-        <span className="text-bold"></span> &nbsp;
-        <a href="http://www.4geeksacademy.com" target="_blank" rel="noopener noreferrer">
-          4Geeks Academy
-        </a>
-      </p>
+      <p className="p-text">© Agenda Inteligente. 2024</p>
     </div>
   </footer>
 );
